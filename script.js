9c2710b8783dc20fa91cfc55d69da927e1100c0f
Henry Navntoft <henrylnavntoft@gmail.com>
2023-03-23 16:26:08 +0100

    Fixed all bugs

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 "use strict";
 
-// get the button and the html element
+// Get the button and the html element
 const themeButton = document.querySelector(".theme_btn");
 const html = document.querySelector("html");
 
-// listen for button clicks
+// Listen for button clicks
 themeButton.addEventListener("click", () => {
-  // toggle the data-theme attribute of the html element
-  if (html.getAttribute("data-theme") === "light") {
-    html.setAttribute("data-theme", "dark");
-  } else {
-    html.setAttribute("data-theme", "light");
-  }
+  // Toggle the data-theme attribute of the html element between "light" and "dark"
+  html.setAttribute(
+    "data-theme",
+    html.getAttribute("data-theme") === "light" ? "dark" : "light"
+  );
 });
